7a1ee79f346dd4a6de35cb5a813f6ef6b633f3eb
andrey kapitovskiy <pepsicooled@mail.ru>
2024-12-04 01:24:42 +0300

    add comment a lot

--- Teledock.Infrastructure/dbContext/Interceptors/MyCustomInterceptorForDates.cs
+++ Teledock.Infrastructure/dbContext/Interceptors/MyCustomInterceptorForDates.cs
@@ -6,13 +6,15 @@ namespace Teledock.Infrastructure.dbContext.Interceptors
 {
     public class MyCustomInterceptorForDates:SaveChangesInterceptor
     {
+        //добавление даты создания записи и добавление/обновление даты обновления записи инкапсулировал в перехватчике события сохранения базы данных
         public override async ValueTask<InterceptionResult<int>> SavingChangesAsync(DbContextEventData eventData, InterceptionResult<int> result, CancellationToken token = default)
         {
             try
             {
+                //находим сущности клиент или фаундер на состояние добавление или обновления
                 var entries = eventData.Context.ChangeTracker.Entries().Where(e => e.Entity is Client && (e.State == EntityState.Added || e.State == EntityState.Modified) ||
                 e.Entity is Founder && (e.State == EntityState.Added || e.State == EntityState.Modified));
-
+                //пробегаемся по каждой сущности и если состояние добавления добавляем к записи дату создания если же обновление обновляем запись даты обновления сущности
                 foreach (var entry in entries)
                 {
                     if (entry.State == EntityState.Added)
